b16af32a532a6c667ac98fe63cbfac5de6438005
ahmed <a.moustafa@penta-b.com>
2020-09-26 10:42:06 +0200

    init first git repo

---
+++ index.js
@@ -0,0 +1,21 @@
+'' // string
+ 
+5 // int
+
+2.57 // float
+
+true // boolean
+
+var result = undefined;
+
+var res = null
+
+var val1 = 5
+
+var val2 = 2
+
+var a = 1234
+
+var b = 1234
+
+console.log(7 % 5)
